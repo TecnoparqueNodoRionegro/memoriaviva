11bc7ce2556626fd171945929afd6167cf1cc377
andresgil2809 <andresgil3010@hotmail.com>
2023-03-30 15:45:14 -0500

    Avance

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,76 @@
+<div>
+    {{-- <nav class="navbar navbar-expand-lg bg-dark" style="height: 10rem; font-family: font-family: 'Roboto', sans-serif;">
+        <div class="h-100 p-3 px-5 container-fluid shadow fw-bold fs-5">
+            <img class="h-100" src="{{ URL::asset('storage/img/logo.png') }}">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="nav ">
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Murales</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Artistas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Nosotros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Tienda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Contáctanos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Descargar APP</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav> --}}
+    <nav class="navbar navbar-expand-lg bg-dark shadow" style="height: 10rem; font-family: font-family: 'Roboto', sans-serif;">
+        <div class="container-fluid fw-bold fs-5">
+          <a class="navbar-brand" href="#">
+            <img src="{{ URL::asset('storage/img/logo.png') }}" width="200">
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fa-solid fa-bars text-white"></i>
+          </button>
+          <div class="collapse navbar-collapse position-relative" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 position-absolute end-0 mx-5">
+                <li class="nav-item">
+                    <a class="nav-link text-white" aria-current="page" href="#">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Murales</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Artistas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Nosotros</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Tienda</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Contáctanos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Descargar APP</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+                </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+</div>
