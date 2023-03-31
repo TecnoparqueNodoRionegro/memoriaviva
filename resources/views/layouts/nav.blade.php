d0676a12f6947f4c47cdca213da768041bdddb75
andresgil2809 <andresgil3010@hotmail.com>
2023-03-31 10:23:59 -0500

    navbar2

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,40 +1,42 @@
 <div>
-    <nav class="navbar navbar-expand-lg  shadow" style="height: 10rem; font-family: font-family: 'Roboto', sans-serif; background-color: black;">
+    <nav class="navbar navbar-expand-lg" style="height: 10rem; font-family: font-family: 'Roboto', sans-serif; background-color: black; box-shadow: 0 .125rem .25rem rgba(218, 218, 218, 0.25)">
         <div class="container-fluid fw-bold fs-5">
-          <a class="navbar-brand" href="#">
-            <img src="{{ URL::asset('storage/img/logo.png') }}" width="200">
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <i class="fa-solid fa-bars text-white"></i>
-          </button>
-          <div class="collapse navbar-collapse position-relative" id="navbarSupportedContent">
-            <ul class="navbar-nav mb-2 position-absolute end-0 mx-5">
-                <li class="nav-item">
-                    <a class="nav-link text-white" aria-current="page" href="#">Inicio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Murales</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Artistas</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Ruta cultural</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Tienda</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Descargar APP</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#"><i class="fa-solid fa-cart-shopping"></i></a>
-                </li>
-
-            </ul>
-          </div>
+            <div class="col-2 d-flex">
+                <a class="navbar-brand " href="#">
+                    <img src="{{ URL::asset('storage/img/logo.png') }}" width="80%" class="mx-5">
+                </a>
+            </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa-solid fa-bars text-white"></i>
+            </button>
+            <div class="collapse navbar-collapse col-10" id="navbarSupportedContent">
+                <ul class="navbar-nav mb-2 " style="margin-left: 20%;">
+                    <li class="nav-item">
+                        <a class="nav-link text-white" aria-current="page" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Murales</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Artistas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Ruta cultural</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Tienda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Descargar APP</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+                    </li>
+                    <li class="nav-item" style="">
+                        <a class="nav-link text-white" href="#">Ingresar</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-      </nav>
-      <hr>
+    </nav>
 </div>
