1f75b279d43caa29cbc7bfcf9737d024ca52d8ba
andresgil2809 <andresgil3010@hotmail.com>
2023-03-31 09:23:06 -0500

    navbar

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,41 +1,5 @@
 <div>
-    {{-- <nav class="navbar navbar-expand-lg bg-dark" style="height: 10rem; font-family: font-family: 'Roboto', sans-serif;">
-        <div class="h-100 p-3 px-5 container-fluid shadow fw-bold fs-5">
-            <img class="h-100" src="{{ URL::asset('storage/img/logo.png') }}">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="nav ">
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Murales</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Artistas</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Nosotros</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Tienda</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Contáctanos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Descargar APP</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#"><i class="fa-solid fa-cart-shopping"></i></a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav> --}}
-    <nav class="navbar navbar-expand-lg bg-dark shadow" style="height: 10rem; font-family: font-family: 'Roboto', sans-serif;">
+    <nav class="navbar navbar-expand-lg  shadow" style="height: 10rem; font-family: font-family: 'Roboto', sans-serif; background-color: black;">
         <div class="container-fluid fw-bold fs-5">
           <a class="navbar-brand" href="#">
             <img src="{{ URL::asset('storage/img/logo.png') }}" width="200">
@@ -55,22 +19,22 @@
                     <a class="nav-link text-white" href="#">Artistas</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Nosotros</a>
+                    <a class="nav-link text-white" href="#">Ruta cultural</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link text-white" href="#">Tienda</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Contáctanos</a>
-                </li>
+
                 <li class="nav-item">
                     <a class="nav-link text-white" href="#">Descargar APP</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link text-white" href="#"><i class="fa-solid fa-cart-shopping"></i></a>
                 </li>
+
             </ul>
           </div>
         </div>
       </nav>
+      <hr>
 </div>
