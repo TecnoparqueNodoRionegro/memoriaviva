ba74717c4f1f0731af23c8bde9c85f0a27827cdc
yudy <yudyrh17@hotmail.com>
2023-04-26 08:48:22 -0500

    Funcionalidad de menus dinamico

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -14,49 +14,16 @@
                     
                     @foreach ($menus as $menu)
                     
+                    @if ($menu->title == "Carrito")
+                        <li class="nav-item">
+                            <a class="nav-link text-white {{ request()->routeIs('/') ? 'border-bottom border-warning' : ''  }}"  aria-current="page" href="{{route($menu->route)}}"><i class="fa-solid fa-cart-shopping"></i></a>
+                        </li>
+                    @else
                     <li class="nav-item">
                         <a class="nav-link text-white {{ request()->routeIs('/') ? 'border-bottom border-warning' : ''  }}"  aria-current="page" href="{{route($menu->route)}}">{{$menu->title}}</a>
-
-                        @if ( $menu->title =='Carrito')
-                           <a class="nav-link text-white {{ request()->routeIs('/') ? 'border-bottom border-warning' : ''  }}"  aria-current="page" href=""><i class="fa-solid fa-cart-shopping"></i></a> 
-                        @endif
                     </li>
-                    
+                    @endif
                     @endforeach
-                    {{-- <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle text-white" href="{{ route('galleries.index') }}" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Murales
-                        </a>
-                        <ul class="dropdown-menu" style="background: black">
-                            <li class="nav-item">
-                                <a class="nav-link  text-white" aria-current="page" href="{{ route('galleries.index') }}">Galeria</a>
-                            </li>
-                        <li><hr class="dropdown-divider"></li>
-                            <li class="nav-item">
-                                <a class="nav-link  text-white" aria-current="page" href="#artistas">Artistas</a>
-                            </li>
-                        </ul>
-                    </li> --}}
-
-                    {{-- <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Ruta cultural</a>
-                    </li> --}}
-
-                    {{-- <li class="nav-item">
-                        <a class="nav-link text-white {{ request()->routeIs('products.index') ? 'border-bottom border-warning' : '' }}" href="{{ route('products.index') }}">Tienda</a>
-                    </li> --}}
-
-                    {{-- <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Descargar APP</a>
-                    </li> --}}
-
-                    {{-- <li class="nav-item">
-                        <a class="nav-link text-white" href="#"><i class="fa-solid fa-cart-shopping"></i></a>
-                    </li> --}}
-
-                    {{-- <li class="nav-item" style="">
-                        <a class="nav-link text-white" href="{{ url('auth/login') }}">Iniciar</a>
-                    </li> --}}
                 </ul>
             </div>
         </div>
