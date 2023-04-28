78656602ca3bcbbd592eb919c7ca53e5606854dd
Harold Marin <haroldmarin2011@gmail.com>
2023-04-28 17:52:25 -0500

    carrito de compras sin funcionalidad de pasarela de pago

--- app/Http/Controllers/cartController.php
+++ app/Http/Controllers/cartController.php
@@ -13,21 +13,27 @@ class cartController extends Controller
     public function add(Request $request){
        
         $product = Product::find($request->product_id);
-
+        $total = 0;
         Cart::add(
             $product->id,
             $product->name,
             $product->price,
             1,
-            array("url"=>$product->files->route)
+            array("url"=>$product->files->route),
+            $total = $product->price + $total
         );
 
         return back()->with('success',"Se agrego el producto al carrito");
     }
     public function cart(){
-        $params =[
-            'title'=>'Shopping Cart Checkout',
-        ];
-        return View('products.checkout')->with($params);
+       
+        return View('products.checkout');
+    }
+    public function removeitem(Request $request){
+        //$product = Product::where(id, $request->id->firstOrFail();
+        Cart::remove([
+            'id' => $request->id,
+        ]);
+        return back()->with('success',"Producto eliminado del carrito");
     }
 }
--- resources/views/products/checkout.blade.php
+++ resources/views/products/checkout.blade.php
@@ -1,22 +1,51 @@
 @extends('layouts.template')
-
-@section('title', $product->name)
-
 @section('content')
     <body class="bg-light">
-        
-        @extends('layouts.templateProducts')
-        
-        @section('buttons')
-        
-            @if (count(Cart::getContent()))
-            <a href="{{ route('cart.checkout')}}">ver carrito<span class="banger banger-danger">{{ count(Cart::getContent())}}></span></a>
-                
-            @else
-               <p>carrito vacio</p>
-            
-            @endif
-        @endsection
-
+        <div class="container">
+            <div class="row justify-content-center">
+                <h2>Carrito De Compras</h2>
+                @if (count(Cart::getContent()))
+                    <table>
+                        <thead>
+                            <th>ID</th>
+                            <th>NOMBRE</th>
+                            <th>PRECIO</th>
+                            <th>CANTIDAD</th>
+                            <th>TOTAL POR PRODUCTO</th>
+                        </thead>
+                        <tbody>
+                            @php $total = 0 @endphp <!-- Inicializa variable total -->
+                            @foreach(Cart::getContent() as $item)
+                                <tr>
+                                    <td>{{ $item->id }}</td>
+                                    <td>{{ $item->name }}</td>
+                                    <td>$ {{ number_format($item->price) }}</td>
+                                    <td>{{ $item->quantity }}</td>
+                                    <td>${{ number_format(($subtotal=$item->price * $item->quantity))}}</td> <!-- Calcula subtotal de cada producto -->
+                                    <td>
+                                        <form action="{{ route('cart.removeitem')}}" method="post">
+                                            @csrf
+                                            <input type="hidden" name="id" value="{{ $item->id}}">
+                                            <button class="btn btn-outline-danger btn-sm" onclick="return confirm('¿Desea eliminar articulo?');"><i class="fas fa-trash-alt"></i> Eliminar</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                @php $total += $subtotal @endphp <!-- Acumula subtotal en variable total -->
+                            @endforeach
+                        </tbody>
+                    </table>
+                    <div class="text-end">
+                        <h4>Total del carrito: ${{ number_format($total) }}</h4> <!-- Muestra total del carrito -->
+                        <a href="{{ route('pay.index', $total) }}">
+                            <button class="btn btn-dark">Comprar</button>
+                        </a>
+                    </div>
+                @else
+                <div class="alert alert-danger text-center" role="alert">
+                    Carrito vacío
+                </div>
+                @endif
+            </div>
+        </div>
     </body>
 @endsection
