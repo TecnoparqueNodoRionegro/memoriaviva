f65e460ebdeb5329b4f1e4791594909b3d4a79f6
Andres0051 <charles_051@hotmail.com>
2023-05-02 01:13:12 -0500

    cambios 02/05/2023

--- resources/views/galleries/index.blade.php
+++ resources/views/galleries/index.blade.php
@@ -4,138 +4,64 @@
 
 @section('content')
 
-<link rel="stylesheet" href="{{ asset("styles/carrusel-artistas.css") }}">
-<div id="carouselExampleControls" class="carousel" data-bs-ride="carousel">
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <div class="card">
-                <div class="img-wrapper"><img src="storage/img/user1.png" class="d-block w-100" alt="..."> </div>
-                <div class="card-body">
-                    <h5 class="card-title">Card title 1</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <div class="card">
-                <div class="img-wrapper"><img src="storage/img/user1.png" class="d-block w-100" alt="..."> </div>
-                <div class="card-body">
-                    <h5 class="card-title">Card title 2</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <div class="card">
-                <div class="img-wrapper"><img src="storage/img/user1.png" class="d-block w-100" alt="..."> </div>
-                <div class="card-body">
-                    <h5 class="card-title">Card title 3</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <div class="card">
-                <div class="img-wrapper"><img src="storage/img/user1.png" class="d-block w-100" alt="..."> </div>
-                <div class="card-body">
-                    <h5 class="card-title">Card title 3</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <div class="card">
-                <div class="img-wrapper"><img src="storage/img/user1.png" class="d-block w-100" alt="..."> </div>
-                <div class="card-body">
-                    <h5 class="card-title">Card title 3</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <div class="card">
-                <div class="img-wrapper"><img src="storage/img/user1.png" class="d-block w-100" alt="..."> </div>
-                <div class="card-body">
-                    <h5 class="card-title">Card title 3</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
+integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
+crossorigin="anonymous" />
+
+<link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"
+    integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw=="
+    crossorigin="anonymous" />
+
+
+<div class="container" style="margin-top: 5px">
+
+@if ($categoryFiles->isEmpty())
+<div style="height: 30rem;">
+    <h1 class="text-center text-white">Aún no hay galerías</h1>
 </div>
-
-{{-- <hr width=95%  aling="center" style="height: 20px"> --}}
-
-
-{{-- <div class="container-md" style="margin-top: 5px">
-
-  @if ($categoryFiles->isEmpty())
-    <div style="height: 30rem;">
-      <h1 class="text-center text-white">Aún no hay galerías</h1>
+@else
+<div class="galeria">
+    <div class="w-100 h2 pb-2 mx-auto text-secondary border-bottom border-secondary">
+        Albúm de murales
     </div>
-  @else
-    <div class="galeria">
-        <div class="w-100  h2 pb-2  mx-auto text-secondary border-bottom border-secondary">
-            Albúm de murales
-        </div>
-        <div class="row">
-          @foreach ($categoryFiles as $category)
-            <div class="col-lg-4 col-md-6 col-sm-12 mt-3 mb-3">
-              <a href="{{ route('galleries.show', $category) }}" class="text-decoration-none">
-                <div class="card text-secondary">
-                  @if ($files[$loop->iteration-1] == null)
-                    <div class="d-flex align-items-center justify-content-center text-secondary card-header" style="height: 200px;">
-                      <h3>VACÍO</h3>
-                    </div>
-                  @else
-                    <div style="height: 200px;">
-                      <img src="{{ URL::asset($files[$loop->iteration-1]->route) }}" class="w-100 h-100 object-fit-cover card-img-top">
-                    </div>
-                  @endif
-
-                  <div class="card-body">
-                    <i class="fa-solid fa-folder"></i>
-                    <span>{{ $category->description }}</span>
-                  </div>
+    <div class="row">
+        @foreach ($categoryFiles as $category)
+        <div class="col-lg-4 col-md-6 col-sm-12 mt-3 mb-3">
+            <a href="{{ route('galleries.show', $category) }}" class="text-decoration-none">
+            <div class="card text-secondary">
+                @if ($files[$loop->iteration-1] == null)
+                <div class="d-flex align-items-center justify-content-center text-secondary card-header" style="height: 200px;">
+                    <h3>VACÍO</h3>
                 </div>
-              </a>
+                @else
+                <div style="height: 200px;">
+                    <img src="{{ URL::asset($files[$loop->iteration-1]->route) }}" class="w-100 h-100 object-fit-cover">
+                </div>
+                @endif
+
+                {{-- <div class="card-body text-center">
+                <span>{{ $category->description }}</span>
+                </div> --}}
             </div>
-          @endforeach
+            </a>
         </div>
+        @endforeach
     </div>
-  @endif
+</div>
+@endif
 
 
-  <div class="container">
-    <div id="artistas">
+{{-- <div class="container mb-4">
+<div id="artistas">
         <div class="w-100  h2 pb-2  mx-auto text-secondary border-bottom border-secondary">
             Nuestros artistas vinculados
         </div>
+
         <div class="row">
-
-            <div class="col-md-4 col-sm-12 mt-3">
+            <div class="col-md-4 col-sm-12 mt-3 mb-4 ">
                 <div class="card text-center h-70 bg-light" >
-                        <img src="{{ URL::asset('storage/img/user1.png')}}" class="card-img-top mt-4 m-auto rounded-circle " alt="..." style="width: 200px; height: 200px;">
+                        <img src="{{ URL::asset('storage/img/user1.png')}}" class="card-img-top m-auto rounded-circle " alt="..." style="width: 100px; height: 100px;">
                     <div class="card-body">
                         <h5 class="card-title">Nombre del artista</h5>
                         <p class="card-text">Materiales con los que trabaja: Oleo</p>
@@ -144,10 +70,9 @@
                     </div>
             </div>
 
-
-            <div class="col-md-4 col-sm-12 mt-3">
-                <div class="card text-center h-100 bg-light" >
-                        <img src="{{ URL::asset('storage/img/1.jpg')}}" class="card-img-top mt-4 m-auto rounded-circle " alt="..." style="width: 200px; height: 200px;">
+            <div class="col-md-4 col-sm-12 mt-3 mb-4 ">
+                <div class="card text-center h-70 bg-light" >
+                        <img src="{{ URL::asset('storage/img/1.png')}}" class="card-img-top m-auto rounded-circle " alt="..." style="width: 100px; height: 100px;">
                     <div class="card-body">
                         <h5 class="card-title">Nombre del artista</h5>
                         <p class="card-text">Materiales con los que trabaja: Oleo</p>
@@ -156,10 +81,9 @@
                     </div>
             </div>
 
-
-            <div class="col-md-4 col-sm-12 mt-3">
-                <div class="card text-center h-100 bg-light" >
-                        <img src="{{ URL::asset('storage/img/2.jpg')}}" class="card-img-top mt-4 m-auto rounded-circle " alt="..." style="width: 200px; height: 200px;">
+            <div class="col-md-4 col-sm-12 mt-3 mb-4">
+                <div class="card text-center h-70 bg-light" >
+                        <img src="{{ URL::asset('storage/img/2.png')}}" class="card-img-top  m-auto rounded-circle " alt="..." style="width: 100px; height: 100px;">
                     <div class="card-body">
                         <h5 class="card-title">Nombre del artista</h5>
                         <p class="card-text">Materiales con los que trabaja: Oleo</p>
@@ -172,10 +96,146 @@
 </div>
 </div> --}}
 
+<div class="container-fluid">
+    <div class="w-100  h2  pb-2  mx-auto text-secondary border-bottom border-secondary">
+        Nuestros artistas vinculados
+    </div>
+    
+    
+        <div class="row">
+            <div class="col-12 m-auto">
+                <div class="owl-carousel owl-theme">
+                    <div class="item mb-4">
+                        <div class="card shadow " style=" border-style: solid; border-width: 10px;
+                            border-image: linear-gradient(45deg, rgb(18, 10, 51), rgb(156, 37, 243)) 1;">
+                            <img src="{{ URL::asset('storage/img/user1.png')}}"class=" m-auto rounded-circle " alt="..." style="width: 100px; height: 100px;">
+                            <div class="card-body">
+                                <div class="card-title text-center">
+                                    <h5 class="card-title">Nombre del artista</h5>
+                                    <p class="card-text">Materiales con los que trabaja: Oleo</p>
+                                    <a href="#" class="btn"><i class="fa-brands fa-instagram fs-1 me-3"></i> <i class="fa-brands fa-twitter fs-1"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <div class="card shadow"  style=" border-style: solid; border-width: 10px;
+                        border-image: linear-gradient(45deg, rgb(18, 10, 51), rgb(156, 37, 243)) 1;">
+                            <img src="{{ URL::asset('storage/img/user1.png')}}" class="card-img-top m-auto rounded-circle " alt="..." style="width: 100px; height: 100px;">
+                            <div class="card-body">
+                                <div class="card-title text-center">
+                                    <h5 class="card-title">Nombre del artista</h5>
+                                    <p class="card-text">Materiales con los que trabaja: Oleo</p>
+                                    <a href="#" class="btn"><i class="fa-brands fa-instagram fs-1 me-3"></i> <i class="fa-brands fa-twitter fs-1"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="card shadow"  style=" border-style: solid; border-width: 10px;
+                        border-image: linear-gradient(45deg, rgb(18, 10, 51), rgb(156, 37, 243)) 1;">
+                            <img src="{{ URL::asset('storage/img/user1.png')}}" class="card-img-top m-auto rounded-circle " alt="..." style="width: 100px; height: 100px;">
+                            <div class="card-body">
+                                <div class="card-title text-center">
+                                    <h5 class="card-title">Nombre del artista</h5>
+                                    <p class="card-text">Materiales con los que trabaja: Oleo</p>
+                                    <a href="#" class="btn"><i class="fa-brands fa-instagram fs-1 me-3"></i> <i class="fa-brands fa-twitter fs-1"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
-<script src="/public/js/slider-artista.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
+integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
+crossorigin="anonymous"></script>
+
+
+<script>
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+</script>
+
+    {{-- <div id="carouselExampleControls" class="carousel">
+        <div class="row ">
+            <div class="carousel-inner">         
+                <div class="carousel-item active">
+                        <div class="col-md-6 mb-4">
+                            <div class="card w-50 text-center h-50 bg-light">
+                                <div class="img-wrapper"><img src="{{ URL::asset('storage/img/user1.png')}}"  class="card-img-top m-auto rounded-circle " alt="..." style="width: 100px; height: 100px;"> </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Nombre del artista</h5>
+                                    <p class="card-text">Materiales con los que trabaja: Oleo</p>
+                                    <a href="#" class="btn"><i class="fa-brands fa-instagram fs-1 me-3"></i> <i class="fa-brands fa-twitter fs-1"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="col-md-6 mb-4">
+                            <div class="card w-50 text-center h-50 bg-light">
+                                <div class="img-wrapper"><img src="{{ URL::asset('storage/img/user1.png')}}"  class="card-img-top m-auto rounded-circle " alt="..." style="width: 100px; height: 100px;"> </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Nombre del artista</h5>
+                                    <p class="card-text">Materiales con los que trabaja: Oleo</p>
+                                    <a href="#" class="btn"><i class="fa-brands fa-instagram fs-1 me-3"></i> <i class="fa-brands fa-twitter fs-1"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="col-md-6 mb-4">
+                            <div class="card w-50 text-center h-50 bg-light">
+                                <div class="img-wrapper"><img src="{{ URL::asset('storage/img/user1.png')}}"  class="card-img-top m-auto rounded-circle " alt="..." style="width: 100px; height: 100px;"> </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Nombre del artista</h5>
+                                    <p class="card-text">Materiales con los que trabaja: Oleo</p>
+                                    <a href="#" class="btn"><i class="fa-brands fa-instagram fs-1 me-3"></i> <i class="fa-brands fa-twitter fs-1"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button> --}}
+
+            </div>
+        </div>
+    </div>
+</div>
+<div style="width: 100%; height: 15rem; position: absolute;">
+    <img class="w-100 h-100 object-fit-cover float-stard" src="{{ URL::asset('storage/img/fondo.png') }}">
+</div>
+
 {{-- @include('layouts.footer') --}}
-
 
 @endsection
