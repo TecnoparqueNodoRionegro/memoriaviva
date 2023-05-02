9affc3bec9ff5d1d59676e0fb61c8e51108c96d3
charles_051@hotmail.com <TECNOPARQUE@RIODFPREAGOD291.SENA.RED>
2023-05-02 08:55:05 -0500

    cambios 02/05/2023

--- resources/views/galleries/show.blade.php
+++ resources/views/galleries/show.blade.php
@@ -50,28 +50,38 @@
       @endif
     </div>
   </section> --}}
-  <link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
 
-  <div class="container">
-  <style>
-    img{
-      max-width: 100%;
-    }
-    .gallery{
-      background-color: #dbddf1;
-      padding: 80px 0;
-    }
-    .gallery img{
-      background-color: #ffffff;
-      padding: 15px;
-      width: 100%;
-      box-shadow: 0 0 15px rgba(0,0,0,0.3);
-      cursor: pointer;
-    }
-    #gallery-modal .modal-img{
-      width: 100%;
-    }
-  </style>
+
+<style>
+
+img{
+    transition: 300ms;
+}
+
+img:hover{
+    opacity: 0.5;
+}
+
+img{
+    max-width: 100%;
+}
+.gallery{
+    padding: 80px 0;
+}
+.gallery img{
+    background-color: rgb(228, 228, 228);
+    padding: 15px;
+    width: 100%;
+    box-shadow: 0 0 15px rgba(0,0,0,0.3);
+    cursor: pointer;
+}
+#gallery-modal .modal-img{
+    width: 100%;
+}
+</style>
+
+<div class="container">
 
   @if ($files->isEmpty())
   <div style="height: 20rem;">
@@ -102,7 +112,8 @@
             </div>
             <div class="col">
               @foreach ($files4 as $file4)
-              <img src="{{ URL::asset($file4->route) }}" class="w-100 rounded object-fit-cover m-1 gallery-item" alt="gallery">
+              <img src="{{ URL::asset($file4->route) }}" class="w-100 rounded object-fit-cover m-1 gallery-item" alt="gallery" style=" border-style: solid; border-width: 10px;
+              border-image: linear-gradient(45deg, rgb(18, 10, 51), rgb(156, 37, 243)) 1;">
               @endforeach
             </div>
         </div>
@@ -112,14 +123,13 @@
   <!-- Modal -->
   <div class="modal fade" id="gallery-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered modal-lg">
-    <div class="modal-content">
+    <div class="modal-content" style=" border-style: solid; border-width: 10px;
+    border-image: linear-gradient(45deg, rgb(18, 10, 51), rgb(156, 37, 243)) 1;">
       <div class="modal-header">
         <!-- <h5 class="modal-title" id="exampleModalLabel">Modal title</h5> -->
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body"  style=" border-style: solid; border-width: 10px;
-      border-image: linear-gradient(45deg, rgb(18, 10, 51), rgb(156, 37, 243)) 1;">
-      
+      <div class="modal-body">
           <img src="img/1.jpg" class="modal-img" alt="modal img">
       </div>
     </div>
